2b0ff148e1f3baaf52214dd87cb781cc9122e6b1
Laurens <laurens.vijnck@selligent.com>
2021-03-14 14:40:45 +0100

    consolidate configs

--- DynamicETL/scripts/001_run.sh
+++ DynamicETL/scripts/001_run.sh
@@ -1,13 +1,14 @@
+# FUTURE: Remove when proto-to-bq no longer requires this!
 export GOOGLE_APPLICATION_CREDENTIALS=/Users/lvijnck/Documents/google-cloud-sdk/geometric-ocean-284614-77fba73ca7b0.json
 
 # Activate venv
+# FUTURE: Create venv as part of the plugin
 VENV_PATH=/Users/lvijnck/Desktop/env/ProtoToBQ/bin/activate
 source $VENV_PATH
 
-# Add path to executable to path variable
-# FUTURE: Create venv as part of the plugin
-SRC_DIR=/Users/lvijnck/Desktop/ProtoBQGeneration/ProtoToBQ/
-export PATH=$SRC_DIR:$PATH
+# Add path to executable Proto-to-bq plugin to path variable
+# Proto will automatically detect the plugin due to the file format
+export PATH=$1:$PATH
 
 DST_DIR=target/generated-sources/protobuf
 
@@ -16,12 +17,13 @@ mkdir -p $DST_DIR/java
 mkdir -p $DST_DIR/bigquery
 
 # Compile protos
-protoc -I=. \
+protoc -I=$1 \
     --java_out=$DST_DIR/java \
     --experimental_allow_proto3_optional \
-    $1
+    $1/$2
 
-# Invoke Custom BigQuery plugin
-protoc $1 \
+### Invoke Custom BigQuery plugin
+protoc -I=$1 \
   --experimental_allow_proto3_optional \
-  --bq_out=$DST_DIR/bigquery
+  --bq_out=$DST_DIR/bigquery \
+   $1/$2
